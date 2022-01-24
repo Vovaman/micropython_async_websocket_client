541d521fa4297401d9cd7aa405da29b3b2632915
vovaman <bd_postbox1@mail.ru>
2022-01-24 20:00:35 +0300

    package doesn't export class

--- setup.py
+++ setup.py
@@ -1,15 +1,11 @@
-
-from setuptools import setup, find_packages
-#import sdist_upip
-
-
 import sys
+
 # Remove current dir from sys.path, otherwise setuptools will peek up our
 # module instead of system's.
 sys.path.pop(0)
-from setuptools import setup
+import setuptools
+
 sys.path.append("./sdist_upip")
-sys.path.append("..")
 import sdist_upip
 
 with open('README.md') as readme_file:
@@ -18,24 +14,27 @@ with open('README.md') as readme_file:
 with open('HISTORY.md') as history_file:
     HISTORY = history_file.read()
 
-setup_args = dict(
-    name='micropython-async_websocket_client',
-    version='0.1.0',
+install_requires = []
+
+setuptools.setup(
+    name="micropython-async_websocket_client",
+    version="0.1.4",
     description='Asynchronous websocket client for ESP32 controller.',
     long_description_content_type="text/markdown",
     long_description=README + '\n\n' + HISTORY,
     license='Apache License 2.0',
-    packages=find_packages(),
     author='Vladimir Badashkin',
     author_email='bd_postbox1@mail.ru',
     keywords=['ESP32', 'micropython', 'websocket', 'asynchronous', 'client'],
     url='https://github.com/Vovaman/micropython_async_websocket_client',
-    download_url='https://pypi.org/project/micropython_async_websocket_client/',
-    py_modules=["async_websocket_client"],
-    cmdclass={'sdist': sdist_upip.sdist}
+    download_url='https://github.com/Vovaman/micropython_async_websocket_client/releases',
+    cmdclass={'sdist': sdist_upip.sdist},
+    install_requires=install_requires,
+    classifiers=[
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"async_websocket_client": "async_websocket_client"},
+    packages=["async_websocket_client"]
+    
 )
-
-install_requires = []
-
-if __name__ == '__main__':
-    setup(**setup_args, install_requires=install_requires)
